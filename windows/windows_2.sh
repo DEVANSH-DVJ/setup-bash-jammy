0a7c7d6f3e0232d5d5c529f491f6ff730d99c4f8
devansh-dvj <devansh.dvj@gmail.com>
2022-06-27 00:03:23 +0530

    Added windows_2.sh

---
+++ windows/windows_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Now, you can configure Music from Windows and below are some important links created
+ln -s ~/Windows-D/EVERYTHING/ ~/Desktop/EVERYTHING;
+ln -s ~/Windows-D/EVERYTHING/IITB/ ~/Desktop/IITB;
+ln -s ~/Windows-D/EVERYTHING/BOOKS/ ~/Desktop/BOOKS;
+ln -s ~/Windows-D/EVERYTHING/PROJECTS/ ~/Desktop/PROJECTS;
