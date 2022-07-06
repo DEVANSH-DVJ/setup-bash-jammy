ca113287bed495fa40645fb4f4a7261acc65b354
devansh-dvj <devansh.dvj@gmail.com>
2022-07-06 21:32:31 +0530

    Added Node and npm

--- basic_tools.sh
+++ basic_tools.sh
@@ -1,2 +1,8 @@
 # Common tools
 sudo apt -y install terminator tree bash-completion;
+
+# Node & NPM
+sudo apt -y install curl;
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -;
+sudo apt -y install nodejs;
+
