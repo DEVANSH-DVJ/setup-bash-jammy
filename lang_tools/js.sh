e77676706a6f7e18b14e8c223867129a5ced2706
devansh-dvj <devansh.dvj@gmail.com>
2022-07-12 23:27:02 +0530

    Added Node and Yarn to lang_tools

---
+++ lang_tools/js.sh
@@ -0,0 +1,10 @@
+## Node & NPM
+sudo apt -y install curl;
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -;
+sudo apt -y install nodejs;
+
+## Yarn
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -;
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list;
+sudo apt update;
+sudo apt -y install yarn;
