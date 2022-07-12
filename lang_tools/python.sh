81cddb2a1b45d823c78c9a733cad2a5bc266c9e0
devansh-dvj <devansh.dvj@gmail.com>
2022-07-13 00:20:26 +0530

    Added Python to lang_tools

---
+++ lang_tools/python.sh
@@ -0,0 +1,5 @@
+## Python
+sudo add-apt-repository ppa:deadsnakes/ppa;
+sudo apt update;
+sudo apt -y install python3.8 python3.8-venv python3.8-tk python-tk;
+sudo apt -y install python3-argcomplete flake8 isort pylint;
