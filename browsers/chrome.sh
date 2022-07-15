3e41408a85888814f291950d7f8be6a70e2e78f3
devansh-dvj <devansh.dvj@gmail.com>
2022-07-16 00:04:13 +0530

    Added chrome to browsers

---
+++ browsers/chrome.sh
@@ -0,0 +1,10 @@
+sudo apt update;
+curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/googlechrom-keyring.gpg;
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrom-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list;
+sudo apt update;
+sudo apt -y install google-chrome-stable;
+
+## Test Chrome once
+# google-chrome;
+
+## Sync the account and customize the settings
