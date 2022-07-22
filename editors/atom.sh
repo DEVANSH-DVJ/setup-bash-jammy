c41440920aa8cafc57d153bb647885b535a2ebdb
devansh-dvj <devansh.dvj@gmail.com>
2022-07-23 01:47:32 +0530

    Added atom packages

--- editors/atom.sh
+++ editors/atom.sh
@@ -7,6 +7,25 @@ sudo apt -y install atom;
 ## Test Atom once
 # atom .;
 
+## Install Packages
+apm install atom-beautify;
+apm install atom-latex;
+apm install build;
+apm install busy-signal;
+apm install docblockr;
+apm install emmet;
+apm install file-icons;
+apm install goto-definition;
+apm install multi-cursor;
+apm install pdf-view;
+apm install pigments;
+apm install language-latex;
+apm install latex;
+apm install latex-autocomplete;
+apm install latextools;
+apm install minimap;
+apm install spacegray-atom-dark-syntax;
+
 ## Update config
 echo '"*":
   "atom-beautify":
