371b4f1e7db188ec9d12f130c132b0ea7aae2378
devansh-dvj <devansh.dvj@gmail.com>
2022-07-30 23:42:52 +0530

    Updated vpn setup

--- optionals/vpn_setup.sh
+++ optionals/vpn_setup.sh
@@ -1,3 +1,5 @@
 sudo apt update;
 sudo apt -y upgrade;
 sudo apt -y install network-manager-openvpn network-manager-openvpn-gnome;
+
+## For IITB VPN, go to https://www.cc.iitb.ac.in/ and follow the steps
