074b71267a1def02c38e261e60e13b4434acaf1f
devansh-dvj <devansh.dvj@gmail.com>
2023-06-26 21:39:54 +0530

    Updated README: Replaced IITB with UIUC

--- windows/windows_2.sh
+++ windows/windows_2.sh
@@ -2,6 +2,6 @@
 
 ## Now, you can configure Music from Windows and below are some important links created
 ln -s ~/Windows-D/EVERYTHING/ ~/Desktop/EVERYTHING;
-ln -s ~/Windows-D/EVERYTHING/IITB/ ~/Desktop/IITB;
+ln -s ~/Windows-D/EVERYTHING/UIUC/ ~/Desktop/UIUC;
 ln -s ~/Windows-D/EVERYTHING/BOOKS/ ~/Desktop/BOOKS;
 ln -s ~/Windows-D/EVERYTHING/PROJECTS/ ~/Desktop/PROJECTS;
