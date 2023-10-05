39cdebc15eb5acdd250f01498f8a5ac52d30d995
devansh-dvj <devansh.dvj@gmail.com>
2023-10-05 11:14:22 -0500

    Merged common_tools with primary_setup

--- common_tools.sh
+++
@@ -1,8 +0,0 @@
-# Common tools
-sudo apt -y install terminator tree bash-completion;
-
-# Common aliases
-echo "
-alias sb='source ~/.bashrc';
-alias rm='rm -i';
-" >> ~/.bashrc;
--- primary_setup.sh
+++ primary_setup.sh
@@ -7,3 +7,12 @@ apt list --upgradable;
 
 ## Upgrade
 sudo apt -y upgrade;
+
+# Common tools
+sudo apt -y install tree bash-completion terminator;
+
+# Common aliases
+echo "
+alias sb='source ~/.bashrc';
+alias rm='rm -i';
+" >> ~/.bashrc;
