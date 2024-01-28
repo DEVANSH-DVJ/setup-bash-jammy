5e747d5126ffb3be498d65e310898789fa8811e0
devansh-dvj <devansh.dvj@gmail.com>
2024-01-27 23:08:15 -0600

    Added uiuc vpn to optionals

---
+++ optionals/uiuc_vpn_setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get install openconnect network-manager-openconnect-gnome
+
+## For UIUC VPN, go to https://answers.uillinois.edu/illinois/page.php?id=47640 and follow the steps
+## Download tar.gz from https://uci.service-now.com/kb_view.do?sysparm_article=KB0010201
+# tar zxvf anyconnect-linux64-4.10.07061-predeploy-k9.tar.gz
+# cd anyconnect-linux64-4.10.07061/vpn
+# sudo ./vpn_install.sh
