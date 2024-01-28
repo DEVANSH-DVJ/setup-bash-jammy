745171b7e4ed5ebcaed5a81d44d971ba0995c267
devansh-dvj <devansh.dvj@gmail.com>
2024-01-28 02:34:05 -0600

    Replaced python virtualenv with conda env

--- lang_tools/python.sh
+++ lang_tools/python.sh
@@ -6,30 +6,30 @@ sudo apt -y install python3.10 python3.10-venv python3.10-tk;
 sudo apt -y install python3-argcomplete flake8 isort pylint;
 sudo apt -y install python-is-python3;
 
-mkdir -p ~/virtualenvs;
-cd ~/virtualenvs;
+## Miniconda
+# Download the installation script from https://docs.conda.io/projects/miniconda/en/latest/
+mkdir -p ~/miniconda;
+cd ~/miniconda;
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda/miniconda.sh;
+bash ~/miniconda/miniconda.sh -b -u -p ~/miniconda;
+rm -rf ~/miniconda/miniconda.sh;
+~/miniconda/bin/conda init bash;
+source ~/.bashrc;
+conda config --set auto_activate_base false;
+source ~/.bashrc;
 
-## Virtualenv of python3.8
-python3.8 -m venv 3.8;
-source 3.8/bin/activate;
+## Conda environment 3.10
+conda create -y -n 3.10 python=3.10;
+conda activate 3.10;
 pip install --upgrade pip;
 pip install wheel pathlib;
 pip install requests wget;
 pip install numpy scipy matplotlib pandas;
 pip install ipython jupyter astropy;
 pip install flake8 pylint autopep8;
-deactivate;
-
-## Virtualenv of python3.10
-python3.10 -m venv 3.10;
-source 3.10/bin/activate;
-pip install --upgrade pip;
-pip install wheel pathlib;
-pip install requests wget;
-pip install numpy scipy matplotlib pandas;
-pip install ipython jupyter astropy;
-pip install flake8 pylint autopep8;
-deactivate;
+pip cache purge;
+conda deactivate;
+conda clean -y --all;
 
 ## Python aliases
 echo "alias 3='source ~/virtualenvs/3.8/bin/activate';
