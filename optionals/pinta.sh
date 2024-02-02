10a1f74fe3cad28fff9b6088a831e30dc222daa1
Devansh Jain <devansh.dvj@gmail.com>
2024-02-01 18:27:23 -0600

    Updated pinta install script

--- optionals/pinta.sh
+++ optionals/pinta.sh
@@ -1,3 +1,4 @@
-sudo add-apt-repository ppa:pinta-maintainers/pinta-stable # You get an ENTER prompt
-# sudo apt update; # Not needed
-sudo apt -y install pinta;
+sudo snap install pinta;
+# sudo add-apt-repository ppa:pinta-maintainers/pinta-stable # You get an ENTER prompt
+# sudo apt update;
+# sudo apt -y install pinta;
