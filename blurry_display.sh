721d9e913713e75896d63eae3fdf756ed1bee106
Devansh Jain <devansh.dvj@gmail.com>
2024-02-01 18:24:56 -0600

    Fixed sudo bug in blurry display script

--- blurry_display.sh
+++ blurry_display.sh
@@ -3,5 +3,5 @@
 ## Faced in Dell Laptops with another Display set at fractional scaling. The display with fractional scaling gets blurry.
 ## To prevent this, following script is written.
 
-sed -i '/#WaylandEnable=false/c\WaylandEnable=false' /etc/gdm3/custom.conf;
+sudo sed -i '/#WaylandEnable=false/c\WaylandEnable=false' /etc/gdm3/custom.conf;
 sudo reboot;
