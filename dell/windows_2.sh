e86845dd1a7970f86eaea6c43946fd7436b9390f
devansh-dvj <devansh.dvj@gmail.com>
2024-02-05 01:26:44 -0600

    Added windows scripts for Dell and HP

---
+++ dell/windows_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Now, you can configure Music from Windows and below are some important links created
+ln -s ~/Windows/Users/devansh9/Desktop/EVERYTHING/ ~/Desktop/EVERYTHING;
+ln -s ~/Windows/Users/devansh9/Desktop/EVERYTHING/UIUC/ ~/Desktop/UIUC;
+ln -s ~/Windows/Users/devansh9/Desktop/EVERYTHING/BOOKS/ ~/Desktop/BOOKS;
+ln -s ~/Windows/Users/devansh9/Desktop/EVERYTHING/PROJECTS/ ~/Desktop/PROJECTS;
