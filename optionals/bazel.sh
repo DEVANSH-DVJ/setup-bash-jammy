3b86b7fcdb261aff6b9d122dbcda0c102e82bedc
devansh-dvj <devansh.dvj@gmail.com>
2024-02-18 17:49:16 -0600

    Added bazel to optionals

---
+++ optionals/bazel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -i
+
+# Download bazelisk
+sudo wget -O /usr/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64
+
+# Change permissions
+sudo chmod 775 /usr/bin/bazel
+sudo chown $USER /usr/bin/bazel
+sudo chgrp $USER /usr/bin/bazel
