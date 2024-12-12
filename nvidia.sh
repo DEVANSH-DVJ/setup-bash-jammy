5d9fe98055f61c79de37d23f2ebba932b2485d66
devansh-dvj <devansh.dvj@gmail.com>
2024-12-12 17:29:59 +0530

    Updated nvidia driver

--- nvidia.sh
+++ nvidia.sh
@@ -3,6 +3,19 @@
 sudo add-apt-repository ppa:graphics-drivers/ppa;
 sudo apt update;
 
-sudo apt -y install nvidia-driver-545 nvidia-settings;
+echo "Checking for the latest driver version...";
+nvidia-detector;
+
+if [ $? -eq 0 ]; then
+    echo "Installing the latest Nvidia driver...";
+    cd ~;
+    sudo apt -y remove --purge nvidia*;
+    sudo apt -y autoremove;
+    sudo apt -y install `(nvidia-detector)`;
+else
+    echo "No Nvidia driver found!";
+fi
+
+# sudo apt -y install nvidia-driver-560 nvidia-settings;
 
 # Add CUDA setup here
